1f34502e715ff04fa1f70685c7975475184f8a87
Alexangelj <alexangel.j@gmail.com>
2021-03-04 09:14:33 -0800

    Adds connector interface.

--- contracts/connectors/PrimitiveConnector.sol
+++ contracts/connectors/PrimitiveConnector.sol
@@ -39,10 +39,11 @@ import {
 // WETH Interface
 import {IWETH} from "../interfaces/IWETH.sol";
 import {CoreLib} from "../libraries/CoreLib.sol";
+import {IPrimitiveConnector} from "../interfaces/IPrimitiveConnector.sol";
 
 import "hardhat/console.sol";
 
-abstract contract PrimitiveConnector is Context {
+abstract contract PrimitiveConnector is IPrimitiveConnector, Context {
     using SafeERC20 for IERC20; // Reverts when `transfer` or `transferFrom` erc20 calls don't return proper data
 
     IWETH internal _weth;
@@ -76,6 +77,7 @@ abstract contract PrimitiveConnector is Context {
      */
     function checkApproval(address token, address spender)
         public
+        override
         returns (bool)
     {
         if (!_approved[token][spender]) {
@@ -242,15 +244,29 @@ abstract contract PrimitiveConnector is Context {
 
     // ===== View =====
 
-    function getWeth() public view returns (IWETH) {
+    function isApproved(address token, address spender)
+        public
+        view
+        override
+        returns (bool)
+    {
+        return _approved[token][spender];
+    }
+
+    function getWeth() public view override returns (IWETH) {
         return _weth;
     }
 
-    function getPrimitiveRouter() public view returns (IPrimitiveRouter) {
+    function getPrimitiveRouter()
+        public
+        view
+        override
+        returns (IPrimitiveRouter)
+    {
         return _primitiveRouter;
     }
 
-    function getCaller() public view returns (address) {
+    function getCaller() public view override returns (address) {
         return _primitiveRouter.getCaller();
     }
 }
